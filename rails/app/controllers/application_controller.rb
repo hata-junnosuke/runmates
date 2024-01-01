bcc6bde0257a5c60e6a870cce4ec69c14e2815ea
hata-junnosuke <jh19911209@icloud.com>
2024-01-01 10:45:51 +0900

    [Rails]devise実装

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseHackFakeSession
+
+  skip_before_action :verify_authenticity_token
 end
