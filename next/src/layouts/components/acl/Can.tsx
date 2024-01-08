212d998530c1b658e6388b46be83cb8b182bcf98
hata-junnosuke <jh19911209@icloud.com>
2024-01-08 21:51:28 +0900

    [Next] ログインできなくなったのでテンプレート削除

--- next/src/layouts/components/acl/Can.tsx
+++
@@ -1,7 +0,0 @@
-import { AnyAbility } from '@casl/ability'
-import { createContextualCan } from '@casl/react'
-import { createContext } from 'react'
-
-export const AbilityContext = createContext<AnyAbility>(undefined!)
-
-export default createContextualCan(AbilityContext.Consumer)
--- next/src/pages/_document.tsx
+++ next/src/pages/_document.tsx
@@ -1,32 +1,29 @@
-// ** React Import
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import createEmotionServer from '@emotion/server/create-instance'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { Children } from 'react'
+import { RenderPageResult } from 'next/dist/shared/lib/utils'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentInitialProps,
+} from 'next/document'
+import * as React from 'react'
 
-// ** Next Import
+import createEmotionCache from '@/styles/createEmotionCache'
+import theme from '@/styles/theme'
 
-// ** Emotion Imports
-
-// ** Utils Imports
-import { createEmotionCache } from 'src/@core/utils/create-emotion-cache'
-
-class CustomDocument extends Document {
-  render() {
+export default class MyDocument extends Document {
+  render(): JSX.Element {
     return (
-      <Html lang='en'>
+      <Html lang="ja">
         <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          {/* PWA primary color */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
           <link
-            rel='stylesheet'
-            href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <link
-            rel='apple-touch-icon'
-            sizes='180x180'
-            href='/images/apple-touch-icon.png'
-          />
-          <link rel='shortcut icon' href='/images/favicon.png' />
         </Head>
         <body>
           <Main />
@@ -37,37 +34,65 @@ class CustomDocument extends Document {
   }
 }
 
-CustomDocument.getInitialProps = async ctx => {
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with static-site generation (SSG).
+MyDocument.getInitialProps = async (ctx): Promise<DocumentInitialProps> => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
   const originalRenderPage = ctx.renderPage
+
+  // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
+  // However, be aware that it can have global side effects.
   const cache = createEmotionCache()
   const { extractCriticalToChunks } = createEmotionServer(cache)
 
-  ctx.renderPage = () =>
+  ctx.renderPage = (): RenderPageResult | Promise<RenderPageResult> =>
     originalRenderPage({
-      enhanceApp: App => props => (
-        <App
-          {...props} // @ts-ignore
-          emotionCache={cache}
-        />
-      ),
+      enhanceApp:
+        (App: any) =>
+        // eslint-disable-next-line react/display-name
+        (props): JSX.Element => <App emotionCache={cache} {...props} />,
     })
 
   const initialProps = await Document.getInitialProps(ctx)
+  // This is important. It prevents emotion to render invalid HTML.
+  // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
   const emotionStyles = extractCriticalToChunks(initialProps.html)
-  const emotionStyleTags = emotionStyles.styles.map(style => {
-    return (
-      <style
-        key={style.key}
-        dangerouslySetInnerHTML={{ __html: style.css }}
-        data-emotion={`${style.key} ${style.ids.join(' ')}`}
-      />
-    )
-  })
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ))
 
   return {
     ...initialProps,
-    styles: [...Children.toArray(initialProps.styles), ...emotionStyleTags],
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
   }
 }
-
-export default CustomDocument
