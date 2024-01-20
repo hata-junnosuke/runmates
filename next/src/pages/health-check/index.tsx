de8344865f7e8886125c026ca3e7d12848d33057
hata-junnosuke <jh19911209@icloud.com>
2024-01-20 20:25:30 +0900

    [Next]サインアウト実装

--- next/src/pages/health-check/index.tsx
+++ next/src/pages/health-check/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import useSWR from 'swr'
 import { fetcher } from 'src/utils'
 import { useUserState } from 'src/hooks/useGlobalState'
+import Link from 'next/link'
 
 const HealthCheck: NextPage = () => {
   const [user] = useUserState()
@@ -16,7 +17,14 @@ const HealthCheck: NextPage = () => {
     <>
       <div>Rails疎通確認</div>
       <div>レスポンスメッセージ: {data.message}</div>
-      {user.isSignedIn && <div>{user.name}</div>}
+      {user.isSignedIn && 
+        <>
+          <div>{user.name}</div>
+          <Link href="/sign_out">
+              サインアウト
+          </Link>
+        </>
+      }
     </>
   )
 }
