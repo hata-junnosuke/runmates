849877671abf974b9ce276c693ae746ebdb15f3e
hata-junnosuke <jh19911209@icloud.com>
2024-01-27 21:25:00 +0900

    [Next]ユーザー情報更新画面実装

--- next/src/pages/mypage/index.tsx
+++ next/src/pages/mypage/index.tsx
@@ -1,18 +1,22 @@
 // import ArrowBackSharpIcon from '@mui/icons-material/ArrowBackSharp'
 import { LoadingButton } from '@mui/lab'
 import {
-  AppBar,
+
+  // AppBar,
   Box,
   Container,
-  IconButton,
-  Switch,
+
+  // IconButton,
+  // Switch,
   TextField,
-  Toolbar,
-  Typography,
+
+  // Toolbar,
+  // Typography,
 } from '@mui/material'
 import axios, { AxiosError } from 'axios'
 import type { NextPage } from 'next'
-import Link from 'next/link'
+
+// import Link from 'next/link'
 
 // import { useRouter } from 'next/router'
 import { useEffect, useState, useMemo } from 'react'
@@ -25,12 +29,12 @@ import { useUserState, useSnackbarState } from 'src/hooks/useGlobalState'
 import { useRequireSignedIn } from 'src/hooks/useRequireSignedIn'
 import { fetcher } from 'src/utils'
 
-type ArticleProps = {
+type UseDataProps = {
   name: string
   email: string
 }
 
-type ArticleFormData = {
+type UserDataFormData = {
   name: string
   email: string
 }
@@ -41,18 +45,8 @@ const CurrentArticlesEdit: NextPage = () => {
   // const router = useRouter()
   const [user] = useUserState()
   const [, setSnackbar] = useSnackbarState()
-  const [previewChecked, setPreviewChecked] = useState<boolean>(false)
-  const [statusChecked, setStatusChecked] = useState<boolean>(false)
   const [isFetched, setIsFetched] = useState<boolean>(false)
   const [isLoading, setIsLoading] = useState<boolean>(false)
-
-  const handleChangePreviewChecked = () => {
-    setPreviewChecked(!previewChecked)
-  }
-
-  const handleChangeStatusChecked = () => {
-    setStatusChecked(!statusChecked)
-  }
 
   const url = process.env.NEXT_PUBLIC_API_BASE_URL + '/current/user/'
   const { data, error } = useSWR(
@@ -60,7 +54,7 @@ const CurrentArticlesEdit: NextPage = () => {
     fetcher,
   )
 
-  const mydata: ArticleProps = useMemo(() => {
+  const userData: UseDataProps = useMemo(() => {
     if (!data) {
       return {
         name: "",
@@ -74,18 +68,18 @@ const CurrentArticlesEdit: NextPage = () => {
     }
   }, [data])
 
-  const { handleSubmit, control, reset } = useForm<ArticleFormData>({
-    defaultValues: mydata,
+  const { handleSubmit, control, reset } = useForm<UserDataFormData>({
+    defaultValues: userData,
   })
 
   useEffect(() => {
     if (data) {
-      reset(mydata)
+      reset(userData)
       setIsFetched(true)
     }
-  }, [data, mydata, reset])
+  }, [data, userData, reset])
 
-  const onSubmit: SubmitHandler<ArticleFormData> = (data) => {
+  const onSubmit: SubmitHandler<UserDataFormData> = (data) => {
     if (data.name == '') {
       return setSnackbar({
         message: 'ユーザー名が入力されていません',
@@ -114,7 +108,6 @@ const CurrentArticlesEdit: NextPage = () => {
       uid: localStorage.getItem('uid'),
     }
 
- 
     const patchData = { ...data}
 
     axios({
@@ -125,17 +118,17 @@ const CurrentArticlesEdit: NextPage = () => {
     })
       .then(() => {
         setSnackbar({
-          message: '記事を保存しました',
+          message: 'ユーザー情報を更新しました',
           severity: 'success',
-          pathname: '/current/mypage',
+          pathname: '/mypage',
         })
       })
       .catch((err: AxiosError<{ error: string }>) => {
         console.log(err.message)
         setSnackbar({
-          message: '記事の保存に失敗しました',
+          message: 'ユーザー情報の更新に失敗しました',
           severity: 'error',
-          pathname: '/current/mypage',
+          pathname: '/mypage',
         })
       })
     setIsLoading(false)
@@ -150,111 +143,59 @@ const CurrentArticlesEdit: NextPage = () => {
       onSubmit={handleSubmit(onSubmit)}
       sx={{ backgroundColor: '#EDF2F7', minHeight: '100vh' }}
     >
-      <AppBar
-        position="fixed"
-        sx={{
-          backgroundColor: '#EDF2F7',
-        }}
-      >
-        <Toolbar
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}
-        >
-          <Box sx={{ width: 50 }}>
-            <Link href="/current/articles">
-              <IconButton>
-                {/* <ArrowBackSharpIcon /> */}
-              </IconButton>
-            </Link>
-          </Box>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              gap: { xs: '0 16px', sm: '0 24px' },
-            }}
-          >
-            <Box sx={{ textAlign: 'center' }}>
-              <Switch
-                checked={previewChecked}
-                onChange={handleChangePreviewChecked}
-              />
-              <Typography sx={{ fontSize: { xs: 12, sm: 15 } }}>
-                プレビュー表示
-              </Typography>
-            </Box>
-            <Box sx={{ textAlign: 'center' }}>
-              <Switch
-                checked={statusChecked}
-                onChange={handleChangeStatusChecked}
-              />
-              <Typography sx={{ fontSize: { xs: 12, sm: 15 } }}>
-                下書き／公開
-              </Typography>
-            </Box>
-            <LoadingButton
-              variant="contained"
-              type="submit"
-              loading={isLoading}
-              sx={{
-                color: 'white',
-                fontWeight: 'bold',
-                fontSize: { xs: 12, sm: 16 },
-              }}
-            >
-              更新する
-            </LoadingButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
       <Container
         maxWidth="lg"
         sx={{ pt: 11, pb: 3, display: 'flex', justifyContent: 'center' }}
       >
-        {!previewChecked && (
-          <Box sx={{ width: 840 }}>
-            <Box sx={{ mb: 2 }}>
-              <Controller
-                name="title"
-                control={control}
-                render={({ field, fieldState }) => (
-                  <TextField
-                    {...field}
-                    type="text"
-                    error={fieldState.invalid}
-                    helperText={fieldState.error?.message}
-                    placeholder="Write in Title"
-                    fullWidth
-                    sx={{ backgroundColor: 'white' }}
-                  />
-                )}
-              />
-            </Box>
-            <Box>
-              <Controller
-                name="content"
-                control={control}
-                render={({ field, fieldState }) => (
-                  <TextField
-                    {...field}
-                    type="textarea"
-                    error={fieldState.invalid}
-                    helperText={fieldState.error?.message}
-                    multiline
-                    fullWidth
-                    placeholder="Write in Markdown Text"
-                    rows={25}
-                    sx={{ backgroundColor: 'white' }}
-                  />
-                )}
-              />
-            </Box>
+        <Box sx={{ width: 840 }}>
+          <Box sx={{ mb: 2 }}>
+            <Controller
+              name="name"
+              control={control}
+              render={({ field, fieldState }) => (
+                <TextField
+                  {...field}
+                  type="text"
+                  error={fieldState.invalid}
+                  helperText={fieldState.error?.message}
+                  placeholder="ユーザー名を入力してください。"
+                  fullWidth
+                  sx={{ backgroundColor: 'white' }}
+                />
+              )}
+            />
           </Box>
-        )}
+          <Box sx={{ mb: 2 }}>
+            <Controller
+              name="email"
+              control={control}
+              render={({ field, fieldState }) => (
+                <TextField
+                  {...field}
+                  type="text"
+                  error={fieldState.invalid}
+                  helperText={fieldState.error?.message}
+                  placeholder="メールアドレスを入力してください。"
+                  fullWidth
+                  sx={{ backgroundColor: 'white' }}
+                />
+              )}
+            />
+          </Box>
+
+          <LoadingButton
+            variant="contained"
+            type="submit"
+            loading={isLoading}
+            sx={{
+              color: 'white',
+              fontWeight: 'bold',
+              fontSize: { xs: 12, sm: 16 },
+            }}
+          >
+            更新する
+          </LoadingButton>
+        </Box>
       </Container>
     </Box>
   )
