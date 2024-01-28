c8a13baab6865c8a430358ed4cb445414a643747
hata-junnosuke <jh19911209@icloud.com>
2024-01-28 20:14:55 +0900

    [Next]ユーザー認証実装

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -2,11 +2,16 @@ import type { NextPage } from "next"
 import useSWR from "swr"
 import Error from "@/components/Error"
 import Loading from "@/components/Loading"
+import { useUserState } from "@/hooks/useGlobalState"
+import { useRequireSignedIn } from "@/hooks/useRequireSignedIn"
 import { fetcher } from "@/utils"
 
 const Index: NextPage = () => {
+  useRequireSignedIn()
+
+  const [user] = useUserState()
   const url = "http://localhost:3000/api/v1/health_check"
-  const { data, error } = useSWR(url, fetcher)
+  const { data, error } = useSWR(user.isSignedIn && url, fetcher)
 
   if (error) return <Error />
   if (!data) return <Loading />
