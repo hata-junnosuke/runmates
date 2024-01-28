5d748c10150af33c61d7f19e847e5b9706b512e2
hata-junnosuke <jh19911209@icloud.com>
2024-01-28 19:57:02 +0900

    [Next]Lintの修正漏れ

--- next/src/components/Header.tsx
+++ next/src/components/Header.tsx
@@ -16,7 +16,7 @@ import {
 } from "@mui/material"
 import Image from "next/image"
 import Link from "next/link"
-import { useRouter } from "next/router"
+// import { useRouter } from "next/router"
 import { useState } from "react"
 import { useUserState } from "@/hooks/useGlobalState"
 
