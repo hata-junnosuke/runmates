b8f02aa0e4e64b80688cdfa4dcc23ca282ecdc1b
hata-junnosuke <jh19911209@icloud.com>
2025-05-05 17:50:10 +0900

    ヘルスチェック用のエンドポイントを追加し、Pumaの設定を簡素化しました

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # ヘルスチェック用
-  get "ping" => "application#ping"
-
+  get "health_check" => "health_check#index"
   # Defines the root path route ("/")
   # root "posts#index"
 end
