2b1c7a8286094d236856c708624a79afbad6cf08
hata-junnosuke <jh19911209@icloud.com>
2025-05-25 08:21:47 +0900

    rubocopとfactory_bot導入してCI設定

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include DeviseHackFakeSession
-  
+
   def ping
     render json: { message: "pong" }
   end
