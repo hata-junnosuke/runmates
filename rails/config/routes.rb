dcf558e282eb98c1fdb0ed61a25bfcc624c652f1
hata-junnosuke <jh19911209@icloud.com>
2025-05-24 10:52:46 +0900

    [Rails]devise設定

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # ヘルスチェック用
-  get "health_check" => "health_check#index"
-  # Defines the root path route ("/")
-  # root "posts#index"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  namespace :api do
+    namespace :v1 do
+      get "health_check", to: "health_check#index"
+      mount_devise_token_auth_for "User", at: "auth"
+    end
+  end
 end
