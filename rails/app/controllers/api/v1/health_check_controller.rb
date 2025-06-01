b3e042a1e7f8bb306be7526ff052a1f7d6e5097b
hata-junnosuke <jh19911209@icloud.com>
2025-06-01 11:05:15 +0900

    CD設定

--- rails/app/controllers/api/v1/health_check_controller.rb
+++ rails/app/controllers/api/v1/health_check_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::HealthCheckController < Api::V1::BaseController
   def index
-    render json: { message: "Success Health Check!" }, status: :ok
+    render json: { message: "Success Health Check!!!!!!!" }, status: :ok
   end
 end
