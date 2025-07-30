a028101f05701a02c729a8aaa8dc768d2da2a587
hata-junnosuke <jh19911209@icloud.com>
2025-07-30 20:32:11 +0900

    ユーザーモデルから:confirmableモジュールを削除しました。

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
+        #  , :confirmable
   include DeviseTokenAuth::Concerns::User
 
   has_many :running_records, dependent: :destroy
