696db78447b9e48906a727d3428def83361385cb
hata-junnosuke <jh19911209@icloud.com>
2025-08-02 21:29:34 +0900

    fix: RuboCopの自動修正を適用

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -36,9 +36,9 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    
+
     # タイムゾーンを日本時間に設定
-    config.time_zone = 'Tokyo'
+    config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
 
     # Only loads a smaller set of middleware suitable for API only apps.
