d9f6f593dff080d8dd54d8d86711db54e764d322
hata-junnosuke <jh19911209@icloud.com>
2026-01-22 20:26:42 +0900

    Terraformのバージョン固定をマイナー追従に変更

--- terraform/envs/prod/versions.tf
+++ terraform/envs/prod/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "1.14.3"
+  required_version = "~> 1.14"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.27.0"
+      version = "~> 6.27"
     }
   }
 }
